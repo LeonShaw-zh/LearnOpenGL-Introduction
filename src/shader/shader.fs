dd24a8426196251966661cf17c076558a8a04fda
LeonShaw <LeonShaw.zh@gmail.com>
2018-10-30 11:43:14 +0800

    exercise1: draw a face look at the other direction

--- src/shader/shader.fs
+++ src/shader/shader.fs
@@ -8,5 +8,5 @@ uniform sampler2D Texture1;
 uniform sampler2D Texture2;
 
 void main(){
-   FragColor = mix(texture(Texture1, TexCoord), texture(Texture2, TexCoord), 0.2f);
+   FragColor = mix(texture(Texture1, TexCoord), texture(Texture2, vec2(1-TexCoord.s, TexCoord.t)), 0.2f);
 }
